779b76676c2358c44f447c87756c292388ed9fa6
Michael Jackson <mjijackson@gmail.com>
2011-04-09 20:33:46 -0700

    Use 4 spaces indent instead of 2

--- jview.js
+++ jview.js
@@ -1,225 +1,225 @@
 (function (jQuery, undefined) {
 
-  var jQueryFn = jQuery.fn,
-  isFunction = jQuery.isFunction,
-  _init = true;
+    var jQueryFn = jQuery.fn,
+        isFunction = jQuery.isFunction,
+        _init = true;
 
-  function jView() {
-    if (_init && isFunction(this.init)) {
-      this.init.apply(this, arguments);
-    }
-  }
-
-  jView.version = '0.1';
-
-  jView.sup = jQueryFn.init;
-
-  jView.sub = function (init) {
-    var sup = this;
-
-    function sub() {
-      sup.apply(this, arguments);
+    function jView() {
+        if (_init && isFunction(this.init)) {
+            this.init.apply(this, arguments);
+        }
     }
 
-    sub.sup = sup;
-    sub.sub = sup.sub;
-    sub.addEvent = sup.addEvent;
-    sub.removeEvent = sup.removeEvent;
+    jView.version = '0.1';
 
-    _init = false;
-    sub.prototype = new sup;
-    _init = true;
+    jView.sup = jQueryFn.init;
 
-    sub.prototype.constructor = sub;
-    sub.prototype.sup = sup.prototype;
+    jView.sub = function (init) {
+        var sup = this;
 
-    // Default init simply calls super's init with the same args.
-    sub.prototype.init = init || function () {
-      sup.prototype.init.apply(this, arguments);
-    }
-
-    return sub;
-  }
-
-  jView.addEvent = function (name) {
-    this.prototype[name] = function (data, fn) {
-      if (arguments.length > 0) {
-        if (fn == null) {
-          fn = data;
-          data = null;
+        function sub() {
+            sup.apply(this, arguments);
         }
 
-        return this.bind(name, data, fn);
-      }
+        sub.sup = sup;
+        sub.sub = sup.sub;
+        sub.addEvent = sup.addEvent;
+        sub.removeEvent = sup.removeEvent;
 
-      return this.trigger(name);
-    }
-  }
+        _init = false;
+        sub.prototype = new sup;
+        _init = true;
 
-  jView.removeEvent = function (name) {
-    if (isFunction(this.prototype[name])) {
-      delete this.prototype[name];
-    }
-  }
+        sub.prototype.constructor = sub;
+        sub.prototype.sup = sup.prototype;
 
-  jView.prototype = new jView.sup;
-  jView.prototype.constructor = jView;
+        // Default init simply calls super's init with the same args.
+        sub.prototype.init = init || function () {
+            sup.prototype.init.apply(this, arguments);
+        }
 
-  jView.prototype.init = function (width, height, tagName) {
-    tagName = tagName || 'div';
-    jView.sup.call(this, '<' + tagName + '>');
-
-    width = typeof width === "number" ? Math.round(width) : 0;
-    height = typeof height === "number" ? Math.round(height) : width;
-    if (width || height) {
-      this.css({ width: width, height: height });
+        return sub;
     }
 
-    this.dom = this[0];
-  }
+    jView.addEvent = function (name) {
+        this.prototype[name] = function (data, fn) {
+            if (arguments.length > 0) {
+                if (fn == null) {
+                    fn = data;
+                    data = null;
+                }
 
-  // Hijack jQuery.fn.bind and jQuery.fn.one to call the handler in the scope
-  // of the view object (instead of the element).
-  jQuery.each(['bind', 'one'], function (i, name) {
-    jView.prototype[name] = function (type, data, fn) {
-      if (isFunction(this[type]) && this[type] !== jQueryFn[type]) {
-        jQuery([this])[name](type, data, fn);
+                return this.bind(name, data, fn);
+            }
+
+            return this.trigger(name);
+        }
+    }
+
+    jView.removeEvent = function (name) {
+        if (isFunction(this.prototype[name])) {
+            delete this.prototype[name];
+        }
+    }
+
+    jView.prototype = new jView.sup;
+    jView.prototype.constructor = jView;
+
+    jView.prototype.init = function (width, height, tagName) {
+        tagName = tagName || 'div';
+        jView.sup.call(this, '<' + tagName + '>');
+
+        width = typeof width === "number" ? Math.round(width) : 0;
+        height = typeof height === "number" ? Math.round(height) : width;
+        if (width || height) {
+            this.css({ width: width, height: height });
+        }
+
+        this.dom = this[0];
+    }
+
+    // Hijack jQuery.fn.bind and jQuery.fn.one to call the handler in the scope
+    // of the view object (instead of the element).
+    jQuery.each(['bind', 'one'], function (i, name) {
+        jView.prototype[name] = function (type, data, fn) {
+            if (isFunction(this[type]) && this[type] !== jQueryFn[type]) {
+                jQuery([this])[name](type, data, fn);
+                return this;
+            }
+
+            return jQueryFn[name].call(this, type, data, jQuery.proxy(fn, this));
+        }
+    });
+
+    // Hijack jQuery.fn.unbind and jQuery.fn.trigger.
+    jQuery.each(['unbind', 'trigger'], function (i, name) {
+        jView.prototype[name] = function (type) {
+            if (isFunction(this[type]) && this[type] !== jQueryFn[type]) {
+                jQueryFn[name].apply(jQuery([this]), arguments);
+                return this;
+            }
+
+            return jQueryFn[name].apply(this, arguments);
+        }
+    });
+
+    // Hijack jQuery.fn.triggerHandler. Need to do this separately from unbind
+    // and trigger because the return value is different.
+    jView.prototype.triggerHandler = function (type, data) {
+        if (isFunction(this[type]) && this[type] !== jQueryFn[type]) {
+            return jQuery([this]).triggerHandler(type, data);
+        }
+
+        return jQueryFn.triggerHandler.call(this, type, data);
+    }
+
+    jView.prototype.append = function (view) {
+        jQueryFn.append.apply(this, arguments);
+
+        if (view instanceof jView) {
+            view.superview = this;
+        }
+
         return this;
-      }
-
-      return jQueryFn[name].call(this, type, data, jQuery.proxy(fn, this));
     }
-  });
 
-  // Hijack jQuery.fn.unbind and jQuery.fn.trigger.
-  jQuery.each(['unbind', 'trigger'], function (i, name) {
-    jView.prototype[name] = function (type) {
-      if (isFunction(this[type]) && this[type] !== jQueryFn[type]) {
-        jQueryFn[name].apply(jQuery([this]), arguments);
+    jView.prototype.appendTo = function (view) {
+        jQueryFn.appendTo.apply(this, arguments);
+
+        if (view instanceof jView) {
+            this.superview = view;
+        } else {
+            this.superview = undefined;
+        }
+
         return this;
-      }
-
-      return jQueryFn[name].apply(this, arguments);
-    }
-  });
-
-  // Hijack jQuery.fn.triggerHandler. Need to do this separately from unbind
-  // and trigger because the return value is different.
-  jView.prototype.triggerHandler = function (type, data) {
-    if (isFunction(this[type]) && this[type] !== jQueryFn[type]) {
-      return jQuery([this]).triggerHandler(type, data);
     }
 
-    return jQueryFn.triggerHandler.call(this, type, data);
-  }
+    jView.prototype.prepend = function (view) {
+        jQueryFn.prepend.apply(this, arguments);
 
-  jView.prototype.append = function (view) {
-    jQueryFn.append.apply(this, arguments);
+        if (view instanceof jView) {
+            view.superview = this;
+        }
 
-    if (view instanceof jView) {
-      view.superview = this;
+        return this;
     }
 
-    return this;
-  }
+    jView.prototype.prependTo = function (view) {
+        jQueryFn.prependTo.apply(this, arguments);
 
-  jView.prototype.appendTo = function (view) {
-    jQueryFn.appendTo.apply(this, arguments);
+        if (view instanceof jView) {
+            this.superview = view;
+        } else {
+            this.superview = undefined;
+        }
 
-    if (view instanceof jView) {
-      this.superview = view;
-    } else {
-      this.superview = undefined;
+        return this;
     }
 
-    return this;
-  }
+    jView.prototype.before = function (view) {
+        jQueryFn.before.apply(this, arguments);
 
-  jView.prototype.prepend = function (view) {
-    jQueryFn.prepend.apply(this, arguments);
+        if (view instanceof jView) {
+            view.superview = this.superview;
+        }
 
-    if (view instanceof jView) {
-      view.superview = this;
+        return this;
     }
 
-    return this;
-  }
+    jView.prototype.after = function (view) {
+        jQueryFn.after.apply(this, arguments);
 
-  jView.prototype.prependTo = function (view) {
-    jQueryFn.prependTo.apply(this, arguments);
+        if (view instanceof jView) {
+            view.superview = this.superview;
+        }
 
-    if (view instanceof jView) {
-      this.superview = view;
-    } else {
-      this.superview = undefined;
+        return this;
     }
 
-    return this;
-  }
+    jView.prototype.insertBefore = function (view) {
+        jQueryFn.insertBefore.apply(this, arguments);
 
-  jView.prototype.before = function (view) {
-    jQueryFn.before.apply(this, arguments);
+        if (view instanceof jView) {
+            this.superview = view.superview;
+        }
 
-    if (view instanceof jView) {
-      view.superview = this.superview;
+        return this;
     }
 
-    return this;
-  }
+    jView.prototype.insertAfter = function (view) {
+        jQueryFn.insertAfter.apply(this, arguments);
 
-  jView.prototype.after = function (view) {
-    jQueryFn.after.apply(this, arguments);
+        if (view instanceof jView) {
+            this.superview = view.superview;
+        }
 
-    if (view instanceof jView) {
-      view.superview = this.superview;
+        return this;
     }
 
-    return this;
-  }
+    jView.prototype.replaceAll = function (view) {
+        jQueryFn.replaceAll.apply(this, arguments);
 
-  jView.prototype.insertBefore = function (view) {
-    jQueryFn.insertBefore.apply(this, arguments);
+        if (view instanceof jView) {
+            this.superview = view.superview;
+            view.superview = undefined;
+        }
 
-    if (view instanceof jView) {
-      this.superview = view.superview;
+        return this;
     }
 
-    return this;
-  }
+    jView.prototype.replaceWith = function (view) {
+        jQueryFn.replaceWith.apply(this, arguments);
 
-  jView.prototype.insertAfter = function (view) {
-    jQueryFn.insertAfter.apply(this, arguments);
+        if (view instanceof jView) {
+            view.superview = this.superview;
+            this.superview = undefined;
+        }
 
-    if (view instanceof jView) {
-      this.superview = view.superview;
+        return this;
     }
 
-    return this;
-  }
-
-  jView.prototype.replaceAll = function (view) {
-    jQueryFn.replaceAll.apply(this, arguments);
-
-    if (view instanceof jView) {
-      this.superview = view.superview;
-      view.superview = undefined;
-    }
-
-    return this;
-  }
-
-  jView.prototype.replaceWith = function (view) {
-    jQueryFn.replaceWith.apply(this, arguments);
-
-    if (view instanceof jView) {
-      view.superview = this.superview;
-      this.superview = undefined;
-    }
-
-    return this;
-  }
-
-  // expose
-  window.jView = jView;
+    // expose
+    window.jView = jView;
 
 })(jQuery);
